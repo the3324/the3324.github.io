a7a2f70e8496fa4974a84f9f2f447be23188ec9f
3324 <ericverma77@gmail.com>
2026-08-10 14:43:21 +1000

    Delete javascript directory

--- javascript/index.js
+++
@@ -1,7 +0,0 @@
-const box = document.querySelector('.box');
-const box2 = document.querySelector('.box2');
-const box3 = document.querySelector('.box3');
-
-box3.addEventListener('click', () => {
-    box2.style.display = box2.style.display === 'block' ? 'none' : 'block';
-});
